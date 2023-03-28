0e75242804ada048976488c47c4161f2fc96cb83
Alex Lipovka <alex.lipovka@gmail.com>
2023-03-28 17:34:30 +0400

    add image pixels to tiles array

--- 3_mappa_draw/sketch.js
+++ 3_mappa_draw/sketch.js
@@ -1,9 +1,7 @@
-// Объект карты
-let myMap;
-// Холст p5js
-let canvas;
-// Объект mappa
-const mappa = new Mappa('Leaflet');
+let canvas; // Холст p5js
+let myMap; // Объект карты
+const mappa = new Mappa('Leaflet'); // Объект mappa
+
 // Дополнительные настройки карты (координаты центра, масштаб, сервер)
 const options = {
 	lat: 56,
@@ -27,19 +25,19 @@ const options = {
 	// style: 'https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.png'
 };
 
+// Заготовка класса для хранения и описания данных о тайлах
 class tileObj {
-	constructor(index, width, height, color, posx, posy) {
+	
+	constructor(index, width, height, color, posX, posY, img) {
 		this.index = index;
 		this.width = width;
 		this.height = height;
 		this.color = color;
-		this.posx = posx;
-		this.posy = posy;
+		this.posX = posX;
+		this.posY = posY;
+		this.img = img;
 	}
-
 }
-
-let objs = [];
 
 let canvasPos = [0, 0];
 let zoomPos = [0, 0];
@@ -49,6 +47,8 @@ let tileColors = [];
 
 let tiles = [];
 
+let objs = [];
+
 // Настройка приложения
 // Данная функция будет выполнена первой и только один раз
 function setup() {
@@ -57,27 +57,29 @@ function setup() {
 	myMap = mappa.tileMap(options);
 
 	myMap.overlay(canvas);
-	myMap.onChange(onChangedMap);
+	myMap.onChange(onChangedMap); //Вызывает onChangedMap() при сдвиге, масштабировании карты
 }
 
 // Основная функция отрисовки
 // Выполняется 60 раз в секунду (как правило)
 function draw() {
-	// background(100);
 	clear();
+	//знакомимся с тайлами
 	drawGrid();
-	// objs.forEach(element => {
-	// 	let pt = myMap.latLngToPixel(element);
-	// 	ellipse(pt.x, pt.y, 15, 15);
-	// });
-
+	//рисуем свои метки
 	for (let i = 0; i < objs.length; i++) {
 		fill(255. / objs.length * i, 0, 0);
 		let pt = myMap.latLngToPixel(objs[i]);
 		ellipse(pt.x, pt.y, 3 + 5 * i, 3 + 5 * i);
 	}
-
-
+	//общая информация
+	fill(0, 155, 0);
+	textSize(24);
+	let info = 'тайлов ' + tiles.length;
+	text(info, 60, 40);
+	//курсор
+	fill(100, 0, 0, 50);
+	noStroke();
 	ellipse(mouseX, mouseY, 51, 51);
 }
 
@@ -99,7 +101,6 @@ function mouseDragged() {
 
 function onChangedMap() {
 	tiles = [];
-	// console.log('onChangedMap')
 	const regex = /translate\(([-\d]+)px,\s+([-.\d]+)px\)/;
 	const regex3d = /translate3d\(([-\d]+)px,\s+([-.\d]+)px,\s+([-.\d]+)px\)/;
 
@@ -113,7 +114,6 @@ function onChangedMap() {
 	let zoomTrans = zt.match(regex3d);
 	zoomPos[0] = zoomTrans[1];
 	zoomPos[1] = zoomTrans[2];
-	// tileNum = selectAll('.leaflet-tile-loaded').length;
 	tileNum = selectAll('.leaflet-zoom-animated')[curZoom].length;
 	let t = select('#defaultCanvas0').elt.style.transform;
 	const matches = t.match(regex);
@@ -127,18 +127,29 @@ function onChangedMap() {
 		gridTransform.push([gt[1], gt[2]]);
 		tileColors.push([0, 0, 0, 0]);
 		
-		var img = new Image();
+		let img = new Image();
 		img.src = imgs[i].src;
 		loadImage(img.src, image => {
-			var pixels = image.loadPixels();
+			image.loadPixels();
+			let avg = 0;
+			image.pixels.forEach(pix => {
+				avg += pix;
+			});
+			avg /= image.pixels.length;
 			for(let j = 0; j < tiles.length; j++) {
 				if(tiles[j].index == i) {
-					tiles[j].color = image.get(127, 127);
+					c = [	image.pixels[(127*256+127) * 4 + 0], 
+								image.pixels[(127*256+127) * 4 + 1], 
+								image.pixels[(127*256+127) * 4 + 2], 
+								image.pixels[(127*256+127) * 4 + 3]];
+					tiles[j].color = [avg, avg, avg, avg]; //image.get(127, 127); только быстрее
+					tiles[j].img = image;
+					redraw();
+					break;
 				}
 			}
 		});	
 		tiles.push(new tileObj(i, 256, 256, [100, 100, 100, 0], gt[1], gt[2]));
-		// ctx = imgs[i].getContext('2d');
 	}
 
 
@@ -146,28 +157,25 @@ function onChangedMap() {
 		canvasPos[0] = matches3d[1];
 		canvasPos[1] = matches3d[2];
 	}
-	// drawGrid();
-	// console.log(matches);
-	// console.log(matches3d);
 	redraw();
 }
 
 function drawGrid() {
-	// console.log('drawGrid');
+	stroke(150);
+	strokeWeight(0.1);
 	push();
 	translate(Number(canvasPos[0]), Number(canvasPos[1]));
 	push();
 	translate(Number(zoomPos[0]), Number(zoomPos[1]));
 	textSize(12);
 
-	// for (let i = 0; i < gridTransform.length; i++) {
 	for (let i = 0; i < tiles.length; i++) {
 		push();
-		translate(Number(tiles[i].posx), Number(tiles[i].posy));
+		translate(Number(tiles[i].posX), Number(tiles[i].posY));
 		fill(0, 0, 150);
-		let textC = i + ' x' + tiles[i].posx + ' y' + tiles[i].posy + '\n' + 'x' + canvasPos[0] + ' y' + canvasPos[1];
+		let textC = 'tile ' + i + '\npos ' + 'x' + tiles[i].posX + ' y' + tiles[i].posY + '\ncanvas ' + 'x' + canvasPos[0] + ' y' + canvasPos[1] + '\nzoom ' + 'x' + zoomPos[0] + ' y' + zoomPos[1];
 		text(textC, 20, 20);
-			fill(tiles[i].color[0], tiles[i].color[1], tiles[i].color[2], 200);
+			fill(tiles[i].color[0], tiles[i].color[1], tiles[i].color[2], 150);
 		rect(0, 0, 256, 256);
 		pop();
 	}
