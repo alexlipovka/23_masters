221ec49878d19ac7622ad4f7a73957d69b51061d
Alex Lipovka <alex.lipovka@gmail.com>
2023-05-04 18:48:45 +0400

    Coordinate revisit, geo calculus done

--- lite-map/custom_map_webgl/geoconvert.js
+++ lite-map/custom_map_webgl/geoconvert.js
@@ -3,12 +3,12 @@ function geoToTiles(lon, lat, zoom) {
 	let lonRad = lon * Math.PI / 180.;
 	let latRad = lat * Math.PI / 180.;
 	n = Math.pow(2, zoom);
-	let xTile = n * ((lon +180.) / 360.);
-	let yTile = n * (1.-(Math.log(Math.tan(latRad)+1/Math.cos(latRad))/Math.PI))/2.;
+	let xTile = n * ((lon + 180.) / 360.);
+	let yTile = n * (1. - (Math.log(Math.tan(latRad) + 1 / Math.cos(latRad)) / Math.PI)) / 2.;
 	xTile = Math.floor(xTile) % n;
 	yTile = Math.floor(yTile) % n;
 	// console.log(xTile, yTile);
-	return {xTile, yTile, zoom};
+	return { xTile, yTile, zoom };
 }
 
 function screenToGeo() {
@@ -19,4 +19,28 @@ function screenToGeo() {
 	y *= 20000000;//19589163.58;
 	y = (2 * Math.atan(Math.exp(y / Rearth)) - Math.PI / 2) * (180 / Math.PI);
 	return { x, y };
+}
+
+function deg2rad(deg) {
+	return deg * PI / 180;
+}
+
+function rad2deg(rad) {
+	return rad * 180 / PI;
+}
+
+class Geo {
+	zoom = 0;
+	size = 2;
+
+	//Lon in radians
+	xFromLonRad(lon, size = this.size, zoom = this.zoom) {
+		return size / (2 * PI) * Math.pow(2, zoom) * (lon + PI);
+	}
+
+	//Lat in radians
+	yFromLat(lat, size = this.size, zoom = this.zoom) {
+		return size / (2 * PI) * (Math.pow(2, zoom)) * (PI - Math.log(Math.tan(PI / 4 + lat / 2)));
+	}
+
 }
